58016074e4e05985b444c8769e8fdcfea060593c
lee <leessosso@gmail.com>
2025-07-20 22:32:50 +0900

    Firebase 환경 변수를 .env 파일로 추가하고, Firebase 설정을 환경 변수로 변경하여 보안성을 강화했습니다. GitHub Actions 배포 스크립트에 환경 변수를 설정했습니다.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB_QNtdJxuSI-6BfxE6w8OtVwQiIsmeHKM",
-    authDomain: "modoo-menu-a5124.firebaseapp.com",
-    projectId: "modoo-menu-a5124",
-    storageBucket: "modoo-menu-a5124.firebasestorage.app",
-    messagingSenderId: "1030194595835",
-    appId: "1:1030194595835:web:683e783cc913bee162ed5e",
-    measurementId: "G-GEHPPL8VN9"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
